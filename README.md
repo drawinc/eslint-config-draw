bfd39b8b6882d1311f83cb26447ce4aaad7a08fa
uki <yuki-shimizu@c-fo.com>
2022-05-08 23:59:09 +0900

    InitialCommit

---
+++ README.md
@@ -0,0 +1,21 @@
+# eslint-config-draw
+
+Shared configuration for Node.js (TypeScript) in draw projects.
+
+## Installation
+
+```shell
+# npm
+npm install -D -E drawinc/eslint-config-draw
+
+# yarn
+yarn add -D -E drawinc/eslint-config-draw
+```
+
+Then add to your `index.js` like this.
+
+```js
+module.exports = {
+  extends: ['eslint-config-draw'],
+};
+```
---
+++ index.js
@@ -0,0 +1,64 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', 'import'],
+  settings: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
+  rules: {
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+          'type',
+        ],
+        alphabetize: {
+          order: 'asc',
+        },
+        'newlines-between': 'always',
+      },
+    ],
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+  },
+};
